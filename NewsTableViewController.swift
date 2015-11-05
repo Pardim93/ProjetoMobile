06eb6e2c61df2539576a9973c61d063bc100ac4d
wellpardim <wellingtons_10@hotmail.com>
2015-11-05 14:03:25 -0200

    local commit

--- NewsTableViewController.swift
+++ NewsTableViewController.swift
@@ -17,8 +17,8 @@ class NewsTableViewController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
         
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        //         self.navigationItem.rightBarButtonItem = self.editButtonItem()
+//         Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+                 self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     
