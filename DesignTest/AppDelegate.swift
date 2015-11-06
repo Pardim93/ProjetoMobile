7e4b86337fdd7712e96a495735932bc1d826af9e
wellpardim <wellingtons_10@hotmail.com>
2015-11-06 19:49:06 -0200

    fix delegate

--- DesignTest/AppDelegate.swift
+++ DesignTest/AppDelegate.swift
@@ -26,8 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        self.beginProvas()
 //        self.beginProva()
 //        self.beginFinishRegistro()
-//        self.verifyLogin()
-        self.beginAvaliacao()
+        self.verifyLogin()
+//        self.beginAvaliacao()
         
 //        self.beginInserir()
         
