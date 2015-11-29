fc2956545b7da5811aa1bb49bc820fdab6bfa423
André Lucas <AndreLucas@MackBooooook.local>
2015-11-29 19:56:51 -0200

    Ajustes

--- DesignTest/ListaProvaTableViewCell.swift
+++ DesignTest/ListaProvaTableViewCell.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ListaProvaTableViewCell: UITableViewCell {
+class ListaProvaTableViewCell: UITableViewCell, EDStarRatingProtocol {
     
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var disciplinasTextView: CellTextView!
-    @IBOutlet weak var imagemView: UIImageView!
     @IBOutlet weak var numQuestoesLabel: UILabel!
+    @IBOutlet weak var starRating: EDStarRating!
+    
     
     var prova: PFObject?
     let colors:[UIColor] = [UIColor.colorWithHexString("#F7802A", alph: 1.0), UIColor.colorWithHexString("#1F7CFF", alph: 1.0), UIColor.newGreenColor()]
@@ -21,6 +22,7 @@ class ListaProvaTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.configStars()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -31,8 +33,8 @@ class ListaProvaTableViewCell: UITableViewCell {
     
 //    MARK: Set
     func setColor(row: Int){
-        let colorPick = row%3
-        self.tituloLabel.backgroundColor = self.colors[colorPick]
+//        let colorPick = row%3
+//        self.tituloLabel.backgroundColor = self.colors[colorPick]
     }
     
     func setNewProva(novaProva: PFObject, disciplinas: String){
@@ -54,6 +56,8 @@ class ListaProvaTableViewCell: UITableViewCell {
         self.disciplinasTextView.text = newText
         self.disciplinasTextView.font = UIFont(name: "Avenir Book", size: 16)
         self.disciplinasTextView.scrollEnabled = true
+        self.disciplinasTextView.backgroundColor = UIColor.colorWithHexString("EBEFFF", alph: 0.5)
+        self.disciplinasTextView.layer.cornerRadius = 5
         self.disciplinasTextView.textColor = UIColor.newLightBlueColor()
     }
     
@@ -63,5 +67,18 @@ class ListaProvaTableViewCell: UITableViewCell {
     
     func setImage(){
         
+    }
+    
+//    MARK: Config
+    func configStars(){
+        starRating.delegate = self;
+        starRating.backgroundColor = UIColor.clearColor()
+        starRating.starImage = UIImage(named: "Star-20")
+        starRating.starHighlightedImage = UIImage(named: "Star Filled-20")
+        starRating.maxRating = 5
+        starRating.horizontalMargin = 12;
+        starRating.editable = false
+        starRating.displayMode = UInt(EDStarRatingDisplayFull)
+        starRating.rating = 2.5;
     }
 }
